b45bb05c3720264402f8501acdaa51b9eccac19e
akash-babu <akash.babu@flipkart.com>
2019-09-29 20:03:44 +0530

    updates example to show the usage of optional chaining

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,21 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports["default"] = void 0;var sum = function sum(a, b) {return a + b;};var _default =
+"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports["default"] = void 0;(function () {var _nestedObj$address, _nestedObj$address$co, _nestedObj$address$co2, _nestedObj$address$co3, _nestedObj$wrong, _nestedObj$wrong$prop;
+  var nestedObj = {
+    address: {
+      country: {
+        state: {
+          city: {
+            area: 'BTM' } } } } };
+
+
+
+
+
+
+  console.log('area:', nestedObj === null || nestedObj === void 0 ? void 0 : (_nestedObj$address = nestedObj.address) === null || _nestedObj$address === void 0 ? void 0 : (_nestedObj$address$co = _nestedObj$address.country) === null || _nestedObj$address$co === void 0 ? void 0 : (_nestedObj$address$co2 = _nestedObj$address$co.state) === null || _nestedObj$address$co2 === void 0 ? void 0 : (_nestedObj$address$co3 = _nestedObj$address$co2.city) === null || _nestedObj$address$co3 === void 0 ? void 0 : _nestedObj$address$co3.area);
+  console.log('area:', nestedObj === null || nestedObj === void 0 ? void 0 : (_nestedObj$wrong = nestedObj.wrong) === null || _nestedObj$wrong === void 0 ? void 0 : (_nestedObj$wrong$prop = _nestedObj$wrong.prop) === null || _nestedObj$wrong$prop === void 0 ? void 0 : _nestedObj$wrong$prop.check);
+
+})();
+
+var sum = function sum(a, b) {return a + b;};var _default =
 
 sum;exports["default"] = _default;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+function testOptionalChaining() {
+  const nestedObj = {
+    address: {
+      country: 'india',
+    },
+  };
+
+  console.log('area:', nestedObj?.address?.country);
+  console.log('area:', nestedObj?.wrong?.prop);
+}
+
+testOptionalChaining();
+
 const sum = (a, b) => a + b;
 
 export default sum;
