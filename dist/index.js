9cdf7c31bcebab144f3f52f6e8a4ea2f2c12ce98
akash-babu <akash.babu@flipkart.com>
2019-09-29 20:04:00 +0530

    adds build

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,15 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports["default"] = void 0;(function () {var _nestedObj$address, _nestedObj$address$co, _nestedObj$address$co2, _nestedObj$address$co3, _nestedObj$wrong, _nestedObj$wrong$prop;
+"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports["default"] = void 0;function testOptionalChaining() {var _nestedObj$address, _nestedObj$wrong;
   var nestedObj = {
     address: {
-      country: {
-        state: {
-          city: {
-            area: 'BTM' } } } } };
+      country: 'india' } };
 
 
 
+  console.log('area:', nestedObj === null || nestedObj === void 0 ? void 0 : (_nestedObj$address = nestedObj.address) === null || _nestedObj$address === void 0 ? void 0 : _nestedObj$address.country);
+  console.log('area:', nestedObj === null || nestedObj === void 0 ? void 0 : (_nestedObj$wrong = nestedObj.wrong) === null || _nestedObj$wrong === void 0 ? void 0 : _nestedObj$wrong.prop);
+}
 
-
-
-  console.log('area:', nestedObj === null || nestedObj === void 0 ? void 0 : (_nestedObj$address = nestedObj.address) === null || _nestedObj$address === void 0 ? void 0 : (_nestedObj$address$co = _nestedObj$address.country) === null || _nestedObj$address$co === void 0 ? void 0 : (_nestedObj$address$co2 = _nestedObj$address$co.state) === null || _nestedObj$address$co2 === void 0 ? void 0 : (_nestedObj$address$co3 = _nestedObj$address$co2.city) === null || _nestedObj$address$co3 === void 0 ? void 0 : _nestedObj$address$co3.area);
-  console.log('area:', nestedObj === null || nestedObj === void 0 ? void 0 : (_nestedObj$wrong = nestedObj.wrong) === null || _nestedObj$wrong === void 0 ? void 0 : (_nestedObj$wrong$prop = _nestedObj$wrong.prop) === null || _nestedObj$wrong$prop === void 0 ? void 0 : _nestedObj$wrong$prop.check);
-
-})();
+testOptionalChaining();
 
 var sum = function sum(a, b) {return a + b;};var _default =
 
