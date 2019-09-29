7ec129f8cd86c0bf3646b17040f0b8e81b536a64
akash-babu <akash.babu@flipkart.com>
2019-09-29 16:10:21 +0530

    upgrades babel to 7.x and eslint to 6.x

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,3 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.default = sum;function sum(a, b) {
-  return a + b;
-}
+"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports["default"] = void 0;var sum = function sum(a, b) {return a + b;};var _default =
+
+sum;exports["default"] = _default;
